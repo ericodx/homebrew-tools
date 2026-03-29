828fb535b87e2e25e178dac98a9530073ebd6524
Érico Perez Neto <ericodx@icloud.com>
2026-03-28 22:15:34 -0300

    feat: add swift-mutation-testing formula

---
+++ Formula/swift-mutation-testing.rb
@@ -0,0 +1,22 @@
+class SwiftCpd < Formula
+  desc "Find untested behavior in Swift codebases"
+  homepage "https://github.com/ericodx/swift-mutation-testing"
+  url "https://github.com/ericodx/swift-mutation-testing/releases/download/v1.0.0/swift-mutation-testing-v1.0.0-macos.tar.gz"
+  sha256 "1d166fb1c95a23d8e208b44a25cf4965c8ed8c1699aa3ab96efba07e4300c848"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on :macos
+
+  def install
+    bin.install "swift-cpd"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/swift-cpd --version")
+  end
+end
