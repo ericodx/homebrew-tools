8a7dfe49be280508f4516bcb42e5d2231aba8772
Érico Perez Neto <ericodx@icloud.com>
2026-04-06 08:30:18 -0300

    chore: release swift-mutation-testing v1.1.0

--- Formula/swift-mutation-testing.rb
+++ Formula/swift-mutation-testing.rb
@@ -1,8 +1,8 @@
 class SwiftMutationTesting < Formula
   desc "Find untested behavior in Swift codebases"
   homepage "https://github.com/ericodx/swift-mutation-testing"
-  url "https://github.com/ericodx/swift-mutation-testing/releases/download/v1.0.0/swift-mutation-testing-v1.0.0-macos.tar.gz"
-  sha256 "1d166fb1c95a23d8e208b44a25cf4965c8ed8c1699aa3ab96efba07e4300c848"
+  url "https://github.com/ericodx/swift-mutation-testing/releases/download/v1.1.0/swift-mutation-testing-v1.1.0-macos.tar.gz"
+  sha256 "92476dbe827b54a217f6bbf8be39b37191b24a61826da5c0b5c51336419cb2bf"
   license "MIT"
 
   livecheck do
